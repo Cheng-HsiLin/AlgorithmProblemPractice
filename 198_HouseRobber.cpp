3150239d38f2fe2e9fde181bf34f722a0ad1effe
Cheng-HsiLin <jimmylin1991@gmail.com>
2017-03-15 22:38:57 -0700

    Update 198_HouseRobber.cpp

--- 198_HouseRobber.cpp
+++ 198_HouseRobber.cpp
@@ -8,16 +8,8 @@
 
 #include <iostream>
 #include <vector>
-#include <queue>
 
 using namespace std;
-
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
 
 // class Solution {
 // public:
